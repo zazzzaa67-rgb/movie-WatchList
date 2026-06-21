34da405f0392d5abbd23c8d19b0f23b2dacbab9b
zazzzaa67-rgb <z.azz.z.aa.67@gmail.com>
2026-06-21 23:35:44 +0300

    commit changes

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 import Groq from "groq-sdk"
 import express from "express"
 import cors from "cors"
+import dotenv from "dotenv"
+dotenv.config()
 const app = express()
 const PORT = 5000
 app.use(cors())
 app.use(express.json())
-const  MY_SECRET_KEY = "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"
+const groq = new Groq({
+    apiKey : process.env.GROQ_API_KEY,
+    baseURL=process.env.GROQ_URL,
+})
+const messages = []
 app.get('/api/get-api-key', (req , res) => {
-    res.json({apiKey : MY_SECRET_KEY})
+    const {prompt} = req.body
+    const response = await groq.chat.completions.create({
+        model : "llama3-8b-8192",
+        messages,
+        stream: true,
+        
+    })
 })
 app.listen(PORT , () => console.log("server is working nice"))
 
