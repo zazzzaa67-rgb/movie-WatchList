f9ea3bd646712433b3b00d138521e76d85aef8ec
zazzzaa67-rgb <z.azz.z.aa.67@gmail.com>
2026-06-22 18:54:42 +0300

    Connect frontend to live Vercel API

--- index.js
+++ index.js
@@ -176,7 +176,7 @@ if (sendBtn) {
             </div>`;
 
         try {
-            const res = await fetch("http://localhost:5000/api/chat", {
+            const res = await fetch("https://movie-watch-list-teal.vercel.app/api/chat", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ prompt: userInputValue })
