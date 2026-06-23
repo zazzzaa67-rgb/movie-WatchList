8ea8c4a11bf43f8948210b44a04f3670a5f220c7
zazzzaa67-rgb <z.azz.z.aa.67@gmail.com>
2026-06-23 20:52:04 +0300

    commit changes

--- script.js
+++ script.js
@@ -205,7 +205,7 @@ if (sendBtn && messages && aiInput) {
             </div>`;
 
         try {
-            const res = await fetch("/api/chat", {
+            const res = await fetch("/api", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ prompt: userInputValue })
